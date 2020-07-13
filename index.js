cd5fee9fd39791c4fb96a429ee4accc2365a98d4
santiagomartino <sanmb182@gmail.com>
2020-07-12 21:56:18 -0300

    register y login agregados

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express= require("express");
+const { response } = require("express");
 const app= express();
 
 app.use(express.static("public"))
 
 app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+app.get("/register", (req,res) => {
+    res.sendFile(__dirname + "/views/register.html")
+})
+
+app.get("/login", (req,res) => {
+    res.sendFile(__dirname + "/views/login.html")
+})
+
+app.post("/", (req,res) => {
     res.sendFile(__dirname + "/views/index.html")
 })
 
